f3103ac4a2d39c116b30000694ca272cb3d8d8c3
moriokashingo <uma20056@yahoo.co.jp>
2019-09-19 13:03:50 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,9 @@ class ItemsController < ApplicationController
   end
 
   def show
+    @item = Item.find(params[:id])
+    @user_items = Item.where(user_id: "#{@item.user.id}")
+    @brand_items = Item.where(brand_id: "#{@item.brand.id}")
   end
   
 end

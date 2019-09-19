f19fe9a055aba2e8d52e9ad72672c87c09598e39
moriokashingo <uma20056@yahoo.co.jp>
2019-09-19 13:07:43 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "items#index"
-  resources :items
+  resources :items, only: [:index, :show]
   resources :users
-  resources :cards
+  resources :cards, only: [:show]
   # TODO: ビューの確認用。ルーテイング。配置場所が決まり次第変更予定。
   get 'pending/itembuy' => 'pending#itembuy',as: 'pending/itembuy'
   get 'pending/edit' => 'pending#edit',as: 'pending/edit'

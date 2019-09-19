d74225d30731a14ad8360c317460e14eb473474d
moriokashingo <uma20056@yahoo.co.jp>
2019-09-19 13:51:44 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "items#index"
-  resources :items
+  resources :items, only: [:index,:show]
   resources :users
 
   # TODO: ビューの確認用。ルーテイング。配置場所が決まり次第変更予定。

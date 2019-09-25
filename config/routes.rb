5332df6bad4e2492973de0a6c7b0842c4fa208c6
moriokashingo <uma20056@yahoo.co.jp>
2019-09-25 14:36:22 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,5 @@ Rails.application.routes.draw do
   get 'pending/logout' => 'pending#logout',as: 'pending/logout'
   get 'pending/index' => 'pending#index',as: 'pending/index'
   get 'pending/member_complate' => 'pending#member_complate',as: 'pending/member_complate'
+  get 'pending/myitem' => 'pending#myitem',as: 'pending/myitem'
 end

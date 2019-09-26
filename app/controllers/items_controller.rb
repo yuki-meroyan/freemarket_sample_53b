80685a6e6313dc3044f98c41fd1d57e1315f2c00
moriokashingo <uma20056@yahoo.co.jp>
2019-09-26 16:56:33 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,5 +9,20 @@ class ItemsController < ApplicationController
     @user_items = Item.where(user_id: "#{@item.user.id}")
     @brand_items = Item.where(brand_id: "#{@item.brand.id}")
   end
-  
+
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy  if item.user_id == current_user.id
+    if item.destroy
+      redirect_to root_path
+    else
+      render myitem
+    end 
+  end
+
+  def myitem
+    @item = Item.find(params[:id])
+  end
+
+
 end

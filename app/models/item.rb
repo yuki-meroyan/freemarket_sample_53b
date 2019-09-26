28f1d79a51e095da8762bdcacbb309c0a38f2d1c
moriokashingo <uma20056@yahoo.co.jp>
2019-09-26 16:56:17 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,11 @@
 class Item < ApplicationRecord
 
-  has_many :comments
+  has_many :comments               
   belongs_to :user
   belongs_to :brand
   has_many :item_images             , dependent: :destroy
   belongs_to :category
   has_many :likes                   , dependent: :destroy
-  has_one :transaction_informations , dependent: :destroy
+  has_one :transaction_informations 
   has_ancestry
 end

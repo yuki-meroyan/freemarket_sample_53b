0747e08303a67b00d1ba1ab008d463a524f23ffc
moriokashingo <uma20056@yahoo.co.jp>
2019-09-28 11:35:16 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,17 +1,16 @@
 class ItemsController < ApplicationController
+  bofore_action :set_item, only: [:myitem, :show, :destroy]
 
   def index
     @ladys_items = Item.where(category_id: 1)
   end
 
   def show
-    @item = Item.find(params[:id])
     @user_items = Item.where(user_id: "#{@item.user.id}")
     @brand_items = Item.where(brand_id: "#{@item.brand.id}")
   end
 
   def destroy
-    item = Item.find(params[:id])
     item.destroy  if item.user_id == current_user.id
     if item.destroy
       redirect_to root_path
@@ -21,8 +20,12 @@ class ItemsController < ApplicationController
   end
 
   def myitem
-    @item = Item.find(params[:id])
   end
 
+  private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
 
 end

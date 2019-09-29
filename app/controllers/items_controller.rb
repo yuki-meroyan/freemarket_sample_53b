de79063f25a9f2c42da0c160385e82b522c505c0
moriokashingo <uma20056@yahoo.co.jp>
2019-09-29 23:33:44 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:myitem, :show, :destroy]
+  before_action :set_item, only: [:myitem, :show, :destroy, :edit, :update]
 
   def index
     @ladys_items = Item.where(category_id: 1).order('id ASC').limit(10)
@@ -7,6 +7,21 @@ class ItemsController < ApplicationController
 
   def new
   end
+
+  def edit
+  end
+
+  def update
+    if @item.user_id == current_user.id
+      @item.update(item_params)
+    end
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
 
   def show
     @user_items = Item.where(user_id: "#{@item.user.id}").order('id ASC').limit(6).where.not(id: @item.id)
@@ -29,6 +44,11 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def item_params
+    # params.permit(:content).merge(user_id: current_user.id)
+    params.require(:item).permit(:name,:description,:price,:region,:delivery_fee,:delivery_days,:shipping_method ,{brand_ids: [],category_ids:[]},item_images_attributes:[:image]).merge(user_id: current_user.id)
   end
 
 end

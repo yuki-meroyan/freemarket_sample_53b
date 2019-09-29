b017ce60578d9056996afebff5618a9730fbbc16
moriokashingo <uma20056@yahoo.co.jp>
2019-09-29 23:32:38 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "items#index"
   resources :user_details
-  resources :items, only: [:index, :show, :destroy, :new]
+  resources :items, only: [:index, :show, :destroy, :new, :edit,:update]
 
   resources :users
   resources :cards, only: [:index, :new, :show]

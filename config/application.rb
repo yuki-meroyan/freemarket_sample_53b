a4365f6893eac0c31c59a88ef226813239eef2ce
moriokashingo <uma20056@yahoo.co.jp>
2019-09-29 23:33:12 +0900

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module FreemarketSample53b
   class Application < Rails::Application
+    config.action_view.automatically_disable_submit_tag = false 
 
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2

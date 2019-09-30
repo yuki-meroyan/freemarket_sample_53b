2e6591f58f26f45441e741d2298b7bc649972bc3
moriokashingo <uma20056@yahoo.co.jp>
2019-09-30 22:18:07 +0900

    Update application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require jquery.turbolinks
 //= require rails-ujs
 //= require activestorage
-//= require jquery
-//= require jquery_ujs 
+//= require turbolinks
 //= require_tree .

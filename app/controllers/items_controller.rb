a503bae8e559a81e54bfef44df2d0a09a2b15796
moriokashingo <uma20056@yahoo.co.jp>
2019-10-07 22:03:05 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,23 +9,26 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    @child_categories = Category.where(ancestry: params[:keyword])
     @brands = Brand.all
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
 
   def update
     if @item.user_id == current_user.id
-      # ブランドのif文
       if Brand.find_by(name: params[:brand_id]).present?
         xxx =Brand.find_by(name: params[:brand_id])
-        params[:brand_id] ="#{xxx.id}"
-            # ここまでは変更できてる
       else
-        Brand.create(name: params[:barand_id])
+        Brand.create(name: params[:brand_id])
         xxx =Brand.find_by(name: params[:brand_id])
-        params[:brand_id] ="#{xxx.id}"
       end
-    @item.update(item_params)
+    @item.brand_id ="#{xxx.id}"
+    # binding.pry
     end
+
     if @item.update(item_params)
       redirect_to root_path
     else
@@ -51,14 +54,14 @@ class ItemsController < ApplicationController
   def myitem
   end
 
-  private
+private
 
   def set_item
     @item = Item.find(params[:id])
   end
 
   def item_params
-    params.require(:item).permit(:name,:description,:price,:region,:delivery_fee,:delivery_days,:shipping_method ,:brand_id,:category_id,item_images_attributes:[:image]).merge(user_id: current_user.id)
+    params.require(:item).permit(:name,:description,:price,:region,:delivery_fee,:delivery_days,:shipping_method ,:brand_id,:category_id,item_images_attributes:[:image, :_destroy,:id]).merge(user_id: current_user.id)
   end
 
 end

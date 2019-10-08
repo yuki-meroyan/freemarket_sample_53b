a0156ccfc6eebe4c17d62d45a3d5abdbad25d025
moriokashingo <uma20056@yahoo.co.jp>
2019-10-08 21:55:09 +0900

    Update item_images.js

--- app/assets/javascripts/item_images.js
+++ app/assets/javascripts/item_images.js
@@ -1,10 +1,9 @@
 
 $(function() {
   var images = [];
-  var images_length = images.length
   var inputs  =[];
   var input_area = $('.upload__box');
-  var images_count=$('.edit-item__images__image').length;
+  var images_count = $('.edit-item__images__image').length;
 
   function readURL(input) {
     if (input.files && input.files[0]) {
@@ -24,34 +23,34 @@ $(function() {
       case 6:
       case 11:
           console.log(images_count);
-          $('.upload__box').css({'width':'600px','margin-left': '10px'});
+          $(input_area).css({'width':'600px','margin-left': '10px'});
           break;
       case 2:
       case 7:
           console.log(images_count);
-          $('.upload__box').css({'width':'480px','margin-left': '10px'});
+          $(input_area).css({'width':'480px','margin-left': '10px'});
           break;
       case 3:
       case 8:
           console.log(images_count);
-          $('.upload__box').css({'width':'350px','margin-left': '10px'});
+          $(input_area).css({'width':'350px','margin-left': '10px'});
           break;
       case 4:
       case 9:
           console.log(images_count);
-          $('.upload__box').css({'width':'240px','margin-left': '10px'});
+          $(input_area).css({'width':'230px','margin-left': '10px'});
           break;
       case 5:
       case 10:
           console.log(images_count);
-          $('.upload__box').css({'width':'110px','margin-left': '10px'});
+          $(input_area).css({'width':'100px','margin-left': '10px'});
           break;
-    }
+    };
   }
 
 
 
-  $(document).on('change', '#post_img',function(event) {
+  $(document).on('change', '#post_img,#post_img_last',function(event) {
     var html = `<div class="hidden" id="upload__file__previews">
     <img src=" " class="edit-item__images__image" id="avatar_img_prev" data-image = ${images.length + 1}>
     <div class="edit-item__images__btns">
@@ -63,11 +62,11 @@ $(function() {
     $('#upload__file__previews').removeClass('hidden');
     readURL(this);
     $('#upload__file__previews').addClass('edit-item__images');
-    if(images_count < 5){
-      $('.sell__dropbox__box__inner__nonaka').append(html);
-    }else{
-      $('.sell__dropbox__box__inner__nonaka2').append(html);
-    }
+    // if(images_count < 5){
+      $('.upload__box').before(html);
+    // }else{
+    //   $('.sell__dropbox__box__inner__nonaka2').append(html);
+    // }
     // $(input_area ).before(html);
     images.push(html);
     // console.log(images.length,inputs.length)
@@ -75,13 +74,17 @@ $(function() {
     input_area.prepend(new_input);
     images_count = $('.edit-item__images__image').length;
     // console.log(images_count,"アイテムの数")
-    dpboxSize(images_count)
+    dpboxSize(images_count);
+    // 一番上のインプットだけ押せるように表示。
+    $(input_area).children(":first").css({'display':'block'});
+    // 選択したインプットは消す。
+    $(this).css({'display':'none'});
   });
 
 
 // 配列の確認用
   $('.l__single__head').click(function(){
-    console.log(images.length,inputs.length)
+    console.log(images.length,inputs.length);
   });
 
 
@@ -91,7 +94,6 @@ $(function() {
     e.preventDefault();
     var target = $(this).parent().parent();
     var target_image= target.find('img');
-    // console.log(inputs,images)
     $.each(inputs, function(index, input){
       if ($(input).data('image') == target_image.data('image')){
         $(input).remove();
@@ -99,32 +101,30 @@ $(function() {
         images.splice(index, 1);
         inputs.splice(index, 1);
         if(inputs.length == 0) {
-          $('#post_img').attr({
+          $('#post_img_last,#post_img').attr({
             'data-image': 0
-          })
+          });
           $('#avatar_img_prev').attr({
             'data-image': 0
-          })
+          });
         }
       }
     })
     images_count = $('.edit-item__images__image').length;
-    dpboxSize(images_count)
+    dpboxSize(images_count);
   });
 // もともとあるアイテムに対しての削除ボタン（未完成）
   $('.motomoto_btn').click(function(e){
     e.preventDefault();
     var inputId = $(this).data('id');
-    $(this).prev().prop('value', true);
+    $(this).prev().prev().prop('value', "1");
     $(this).parent().parent().hide();
     var deleteimg = $(this).parent().parent().find('img');
     deleteimg.remove();
     images_count = $('.edit-item__images__image').length;
-    dpboxSize(images_count)
+    dpboxSize(images_count);
   });
 
 
-  // upload__boxのサイズ変更
-  $('.sell__dropbox__box__inner').change(function(){
-  });
+
 });

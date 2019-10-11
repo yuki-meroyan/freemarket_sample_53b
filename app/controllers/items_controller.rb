00c699bda1c0357b65ef8b72e2631d5052681659
moriokashingo <uma20056@yahoo.co.jp>
2019-10-12 00:07:16 +0900

    create login_notice

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -66,7 +66,10 @@ private
 
 
   def move_to_index
-    redirect_to action: :index unless user_signed_in?
+    unless user_signed_in?
+      flash[:notice] = "ログインしてください"
+      redirect_to action: :index
+    end
   end
 
 end

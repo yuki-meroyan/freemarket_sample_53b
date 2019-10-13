c946a7fee7d5f4cb7e212bdbad9e45338ec9e604
moriokashingo <uma20056@yahoo.co.jp>
2019-10-13 21:00:48 +0900

    update seed.rb images
    デプロイ環境でwebpファイルが読みとれないようなので、
    すべてjpg,jpegファイルに変更

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 module FreemarketSample53b
   class Application < Rails::Application
-    config.action_view.automatically_disable_submit_tag = false 
+    config.action_view.automatically_disable_submit_tag = false
     config.i18n.default_locale = :ja
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2

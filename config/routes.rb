24faa4402192c058332fff3a47a6e30b1c861184
moriokashingo <uma20056@yahoo.co.jp>
2019-10-20 23:57:36 +0900

    Update routes.rb
     resources :itemsが重複しているので修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "items#index"
 
-  resources :items, only: [:index, :show, :destroy, :new]
   resources :users
   resources :user_details, only:[:show, :edit, :update]
   resources :cards, only: [:index, :new, :show, :create, :destroy]

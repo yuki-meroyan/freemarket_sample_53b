994d3b9689cbd717c3a9f9cfed814d8b5207901f
moriokashingo <uma20056@yahoo.co.jp>
2019-10-23 01:38:02 +0900

    retouch_item_server
    商品編集ページで最初に既存のレコードが表示されるように実装。（途中まで）

--- app/assets/javascripts/item_category.js
+++ app/assets/javascripts/item_category.js
@@ -1,4 +1,8 @@
 $(function () {
+  // dataでカテゴリーの自身、親、親の親のidを送っているので変数に入れる
+  grandparent = $("#category__box__grandchild").data('grandparent')
+  parent      = $("#category__box__grandchild").data('parent')
+  main        = $("#category__box__grandchild").data('main')
 
   var childChangeSelect = function (id, nextSelect) {
     var item_id = $('.sell__upload__head').data('item-id');
@@ -10,9 +14,6 @@ $(function () {
     })
     .done(function (categories) {
       nextSelect.empty();
-      nextSelect.append(`
-        <option value="">---</option>
-        `);
       $(categories).each(function (i,category) {
         nextSelect.append(`
         <option value="${category.id}">${category.name}</option>
@@ -33,12 +34,9 @@ $(function () {
       dataType: 'json'
     })
     .done(function (categories) {
-      nextSelect.empty();
-      nextSelect.append(`
-        <option value="">---</option>
-        `);
+      $("#item_category_id").empty();
       $(categories).each(function (i,category) {
-        nextSelect.append(`
+        $("#item_category_id").append(`
         <option value="${category.id}">${category.name}</option>
         `);
       });
@@ -48,30 +46,58 @@ $(function () {
     });
   }
 
-  $('.item__f-select').prepend(`
-    <option value="" >---</option>
-    `).val("");
-
-
-  $('#category_1_category_1').change(function(){
-    var id = $(this).val();
+// 隠されている親セレクトを表示。そのまま親のoptionを持ってくるajaxにつながる。
+  var child_display =function (i) {
+    var id = $(i).val();
     if (id === "") {
-      $('.category__box__child').css('display', 'none');
-      $('.category__box__grandchild').css('display', 'none');
+      $('#category__box__child').css('display', 'none');
+      $('#category__box__grandchild').css('display', 'none');
       return;
     }
-    $('.category__box__child').css('display', 'block');
+    $('#category__box__child').css('display', 'block');
     childChangeSelect(id, $('#category_2_category_2'));
-  });
-
-  $('#category_2_category_2').change(function(){
-    var id = $(this).val();
+  }
+// 隠されているカテゴリーIDセレクトを表示。そのまま孫のoptionを持ってくるajaxにつながる。
+  var grandchild_display =function (i) {
+    var id = $(i).val();
+    console.log(id)
     if (id === "") {
-      $('.category__box__grandchild').css('display', 'none');
+      $('#category__box__grandchild').css('display', 'none');
       return;
     }
-    $('.category__box__grandchild').css('display', 'block');
-    grandchildChangeSelect (id, $('#item_category_id'));
+    $('#category__box__grandchild').css('display', 'block');
+    grandchildChangeSelect (id, '#item_category_id');
+  }
+
+
+  $(window).on("load", function(){
+    if(document.URL.match(/edit/) && document.URL.match(/items/)) {
+      $('.category__boxes').css({'display':'block'});
+// 親の親の値をgrandparentで送られて来た値に
+      $('#category_1_category_1').val(`${grandparent}`);
+// 親の親の値が変わったらchild_displayを呼んで親のoptionを決める。
+        child_display('#category_1_category_1');
+// 上で決めた中からdataで送られてきた値と一致するものを選択する。(valは入っているがビューは入らない。)
+      $('#category_2_category_2').val(`${parent}`);
+// 上と同じ流れ
+        grandchild_display("#category_2_category_2");
+      $('#item_category_id').val(`${main}`);
+    }
+    $('#category_2_category_2').val(`${parent}`);
+  });
+// 親の親カエゴリーが変更した時の処理
+  $('#category_1_category_1').change(function(){
+    child_display(this);
+  });
+// 親カテゴリーが変更した時の処理
+  $('#category_2_category_2').change(function(){
+    grandchild_display(this);
   });
 
+
+  // 実験用これなら動く
+  // $(window).on("click", function(){
+  //   $('#category_2_category_2').val(`${parent}`);
+  //   $('#item_category_id').val(`${main}`);
+  // });
 });

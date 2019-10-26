c5c66f871acd624a6f62d958653800b2c8720a8a
yuki_m <yakan.meroyan@gmail.com>
2019-10-27 00:42:19 +0900

    UPDATE-Category-View

--- app/assets/javascripts/item_category_list.js
+++ app/assets/javascripts/item_category_list.js
@@ -1,58 +1,58 @@
 
-$(function(){
-  $('#nav-category').mouseover(function(e){
-    $('.nav__category__parent').css('display', 'flex'); 
-    $.ajax({
-      type: 'GET',
-      url: `/categories`,
-      data: {keyword: "parent"},
-      dataType: 'json'
-    })
-    .done(function(parents) {
-      $('.nav__category__parent__list').empty();
-      if (parents.length !== 0) {
-        var insertHTML = "";
-        parents.forEach(function(parent){
-          var insertHTML = `<div class="parent-list" data-category-id=${parent.id}>
-                              <li>${parent.category}</li>
-                            </div>`
-          $('.nav__category__parent__list').append(insertHTML);
-        });
-      }
-    })
-    $('.parent-list').mouseover(function() {
-      $(this).css('background-color', 'red');
-      $('.nav__category__child').css('display', 'flex'); 
-      var parent_id = $(this).data('category-id');
-      $.ajax({
-        type: 'GET',
-        url: `/categories`,
-        data: {target_id: parent_id, keyword: "child"},
-        dataType: 'json'
-      })
-      .done(function(childs) {
-        $('.nav__category__child__list').empty();
-        if (childs.length !== 0) {
-          var insertHTML = "";
-          childs.forEach(function(child){
-            console.log(child);
-            var insertHTML = `<div class="child-list" data-category-id=${parent_id}/${child.id}>
-                                <li>${child.category}</li>
-                              </div>`
-            $('.nav__category__child__list').append(insertHTML);
-          });
-        }
-      })
-    });
-    $('.parent-list').mouseout(function() {
-      $(this).css('background-color', 'white');
-    });
+// $(function(){
+//   $('#nav-category').mouseover(function(e){
+//     $('.nav__category__parent').css('display', 'flex'); 
+//     $.ajax({
+//       type: 'GET',
+//       url: `/categories`,
+//       data: {keyword: "parent"},
+//       dataType: 'json'
+//     })
+//     .done(function(parents) {
+//       $('.nav__category__parent__list').empty();
+//       if (parents.length !== 0) {
+//         var insertHTML = "";
+//         parents.forEach(function(parent){
+//           var insertHTML = `<div class="parent-list" data-category-id=${parent.id}>
+//                               <li>${parent.category}</li>
+//                             </div>`
+//           $('.nav__category__parent__list').append(insertHTML);
+//         });
+//       }
+//     })
+//     $('.parent-list').mouseover(function() {
+//       $(this).css('background-color', 'red');
+//       $('.nav__category__child').css('display', 'flex'); 
+//       var parent_id = $(this).data('category-id');
+//       $.ajax({
+//         type: 'GET',
+//         url: `/categories`,
+//         data: {target_id: parent_id, keyword: "child"},
+//         dataType: 'json'
+//       })
+//       .done(function(childs) {
+//         $('.nav__category__child__list').empty();
+//         if (childs.length !== 0) {
+//           var insertHTML = "";
+//           childs.forEach(function(child){
+//             console.log(child);
+//             var insertHTML = `<div class="child-list" data-category-id=${parent_id}/${child.id}>
+//                                 <li>${child.category}</li>
+//                               </div>`
+//             $('.nav__category__child__list').append(insertHTML);
+//           });
+//         }
+//       })
+//     });
+//     $('.parent-list').mouseout(function() {
+//       $(this).css('background-color', 'white');
+//     });
 
-  })
-  // $('#nav-category').mouseout(function () {
-  //   $('.nav__category__parent').css('display', 'none');
-  // });
-});
+//   })
+//   // $('#nav-category').mouseout(function () {
+//   //   $('.nav__category__parent').css('display', 'none');
+//   // });
+// });
   // $('#nav-category').mouseover(function (e) {
   //   $('.nav__category__parent').css('display', 'flex');    
   // });

814f4e6eaa7d3594cd1fa1b02e75372a1b261fcb
moriokashingo <uma20056@yahoo.co.jp>
2019-10-29 00:46:04 +0900

    create model validation item&item_image
    itemとitem_imageそれぞれのモデルにバリデーションを記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,24 @@
 class Item < ApplicationRecord
 
-  has_many :comments               
-  belongs_to :user
-  belongs_to :brand
-  has_many :item_images                           , dependent: :destroy
-  accepts_nested_attributes_for :item_images      , allow_destroy: true, reject_if: :all_blank
-  belongs_to :category
-  has_many :likes                                 , dependent: :destroy
-  has_one :transaction_informations 
+  has_many    :comments
+  belongs_to  :user
+  belongs_to  :brand
+  has_many    :item_images                        , dependent: :destroy
+  accepts_nested_attributes_for :item_images      , allow_destroy: true, reject_if: proc{ |attributes| attributes['image'].blank? }
+  belongs_to  :category
+  has_many    :likes                              , dependent: :destroy
+  has_one      :transaction_informations
   has_ancestry
+
+  validates :name                                 , presence: true, length: { in: 1..40 }
+  validates :description                          , length: { maximum: 1000 }
+  validates :ancestry                             , presence: true
+  validates :price                                , presence: true
+  validates :region                               , presence: true
+  validates :delivery_fee                         , presence: true
+  validates :delivery_days                        , presence: true
+  validates :shipping_method                      , presence: true
+  validates :brand                                , presence: true
+  validates :user                                 , presence: true
+  validates :category                             , presence: true
 end
--- app/models/item_image.rb
+++ app/models/item_image.rb
@@ -1,6 +1,6 @@
 class ItemImage < ApplicationRecord
 
-  belongs_to :item
-  mount_uploader :image, ImageUploader
-
+  belongs_to     :item
+  mount_uploader :image                             , ImageUploader
+  validates      :image                             , presence: true
 end

f1377b5615f5c85a949aa81d5fafc6854d6af8ac
moriokashingo <uma20056@yahoo.co.jp>
2019-10-30 00:27:35 +0900

    Update transaction_informations_controller_spec.rb
    空白部分の削除

--- spec/controller/transaction_informations_controller_spec.rb
+++ spec/controller/transaction_informations_controller_spec.rb
@@ -15,14 +15,14 @@ RSpec.describe TransactionInformationsController, type: :controller do
     # 購入済みはページが遷移するか？
     it "responds item buyed redirect to item_path" do
       sign_in @user
-      get :index, params: { item_id: @item_buyed } 
+      get :index, params: { item_id: @item_buyed }
       expect(response).to redirect_to item_path(id: @item_buyed)
     end
     # 出品者と購入しようとしている人が同一であればページを遷移する
     it "responds Same person redirect to item_path " do
       sign_in @item.user
       get :index, params: { item_id: @item }
-      expect(response).to redirect_to item_path(id: @item)            
+      expect(response).to redirect_to item_path(id: @item)
     end
   end
 end

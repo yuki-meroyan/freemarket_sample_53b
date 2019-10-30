eabde6717e91ca28971b98fb43cbaf0528993321
yuki_m <yakan.meroyan@gmail.com>
2019-10-30 20:29:17 +0900

    UPDATE-Category-View

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -12,23 +12,8 @@ class CategoriesController < ApplicationController
   end
 
   def show
-    binding.pry
-    Item.joins(:categories).where(categories: { id: 1 })
-    items = Category.joins(:items).where(categories: { id: 159} )
-    items = Category.joins(:items).where(categories: { ancestry: "1/144" } )
-    r = Item.includes(:categories).where(id: params[:id])         
-    Item.joins(:categories).select("Item.*").where('Category.id: params[id]')
-    Category.find(params[:id]).joins(:items).preload(:items)
     category = Category.find(params[:id])
-    category = Category.where(ancestry: "1/14")
-    category = Category.where(ancestry: "1/14")
-    # IN (`取得したtaggingのid`) Item.where("id IN (?)", category)
-    # @items = Item.where(category_id: category.id).page(params[:page]).per(40)
-    @items = Item.where(category_id: 159).page(params[:page]).per(40)
-    @item_images = ItemImage.includes(:item_id)
-    
-    # TODO: showのビューが完成するまで飛ばないようにしています
-    # redirect_to categories_path
+    @items = Item.where(category_id: category.subtree).page(params[:page]).per(12)
   end
 
   def ajax_category_action

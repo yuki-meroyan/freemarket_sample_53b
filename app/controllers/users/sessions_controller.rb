a871db19c531da7988cdef1f7e4604834561a56f
moriokashingo <uma20056@yahoo.co.jp>
2019-10-30 15:37:20 +0900

    updeate users/sessions_controller.rb
    ・users/sessions_controller.rbにログイン時recaptchaを押したかどうかで分岐するif文を追加。
    ・routes.rbにusers/sessions_controller.rbを読み込む文を追加。

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,14 +4,19 @@ class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def new
+    super
+  end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    if verify_recaptcha
+      super
+    else
+      self.resource = resource_class.new
+      respond_with_navigational(resource) { render :new }
+    end
+  end
 
   # DELETE /resource/sign_out
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {
     registrations: 'users/registrations',
     passwords: 'users/passwords',
-    omniauth_callbacks: 'users/omniauth_callbacks' 
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    sessions: 'users/sessions'
   }
 
   devise_scope :user do

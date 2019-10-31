f342c405eebd61768c32825b458aa66c070f8ddd
yuki_m <yakan.meroyan@gmail.com>
2019-11-01 00:56:52 +0900

    UPDATE-Category-View

--- app/assets/javascripts/item_category_list.js
+++ app/assets/javascripts/item_category_list.js
@@ -1,85 +1,79 @@
 $(function(){
 
-  var active_id = ""
+  var parent_id = ""
+  var child_id = ""
+  var grandchild_id = ""
 
   $('#nav-category').on('mouseenter', function(){
-    var target_id = $(this).data('category-id');
-    if (target_id != active_id) {
-      $('.nav__category__parent__list').empty();
-      $('.nav__category__child__list').empty();
-      $('.nav__category__grandchild__list').empty();
-      $('.nav__category__parent').css('display', 'flex'); 
-      $.ajax({
-        type: 'GET',
-        url: `/categories`,
-        data: {keyword: "parent"},
-        dataType: 'json'
-      })
-      .done(function(parents) {
-        if (parents.length !== 0) {
-          var insertHTML = "";
-          parents.forEach(function(parent){
-            var insertHTML = `<div class="parent-list" data-category-id=${parent.id}>
-                                <li>
-                                  <a class="category__main__grandchild__link-name" href="/categories/${parent.id}">
-                                    <p>${parent.category}</p>
-                                  </a>
-                                </li>
-                              </div>`
-            $('.nav__category__parent__list').append(insertHTML);
-          });
-        }
-      })
-    }
-
-    $(document).on('mouseenter', '.parent-list', function(){
-      console.log(active_id)
-      $(this).css('background-color', 'red');
-      $(this).css('color', 'white');
-      $('.nav__category__child').css('display', 'flex'); 
-      var parent_id = $(this).data('category-id');
-      if (active_id != parent_id) {
-        $.ajax({
-          type: 'GET',
-          url: `/categories`,
-          data: {target_id: parent_id, keyword: "child"},
-          dataType: 'json'
-        })
-        .done(function(childs) {
-          $('.nav__category__child__list').empty();
-          $('.nav__category__grandchild__list').empty();
-          $('.nav__category__grandchild').css('display', 'none');
-          if (childs.length !== 0) {
-            var insertHTML = "";
-            childs.forEach(function(child){
-              var insertHTML = `<div class="child-list" data-category-id=${parent_id}/${child.id}>
-                                  <li>
-                                    <a class="category__main__grandchild__link-name" href="/categories/${child.id}">
-                                      <p>${child.category}</p>
-                                    </a>
-                                  </li>
-                                </div>`
-              $('.nav__category__child__list').append(insertHTML);
-            })
-          }
-          active_id = parent_id
+    $('.nav__category__parent__list').empty();
+    $('.nav__category__child__list').empty();
+    $('.nav__category__grandchild__list').empty();
+    $('.nav__category__parent').css('display', 'flex'); 
+    $.ajax({
+      type: 'GET',
+      url: `/categories`,
+      data: {keyword: "parent"},
+      dataType: 'json'
+    })
+    .done(function(parents) {
+      if (parents.length !== 0) {
+        var insertHTML = "";
+        parents.forEach(function(parent){
+          var insertHTML = `<div class="parent-list listNo-${parent.id}" data-category-id=${parent.id}>
+                              <li>
+                                <a class="category__main__grandchild__link-name" href="/categories/${parent.id}">
+                                  <p>${parent.category}</p>
+                                </a>
+                              </li>
+                            </div>`
+          $('.nav__category__parent__list').append(insertHTML);
         });
       }
-      
-      $('.parent-list').on('mouseleave', function(){
-        $(this).css('background-color', 'white');
-        $(this).css('color', 'black');
-      });
     })
-
-    $(document).on('mouseleave', '#nav-category', function(){
-      $('.nav__category__parent__list').empty();
-      $('.nav__category__child__list').empty();
-      $('.nav__category__grandchild__list').empty();
-    });
-
   });
-      
+});
+  //   $(document).on('mouseenter', '.parent-list', function(){
+  //     $(this).css('background-color', 'red');
+  //     $(this).css('color', 'white');
+  //     $('.nav__category__child').css('display', 'flex'); 
+  //     var parent_id = $(this).data('category-id');
+  //     if (active_id != parent_id) {
+  //       $.ajax({
+  //         type: 'GET',
+  //         url: `/categories`,
+  //         data: {target_id: parent_id, keyword: "child"},
+  //         dataType: 'json'
+  //       })
+  //       .done(function(childs) {
+  //         $('.nav__category__child__list').empty();
+  //         $('.nav__category__grandchild__list').empty();
+  //         $('.nav__category__grandchild').css('display', 'none');
+  //         if (childs.length !== 0) {
+  //           var insertHTML = "";
+  //           childs.forEach(function(child){
+  //             var insertHTML = `<div class="child-list" data-category-id=${parent_id}/${child.id}>
+  //                                 <li>
+  //                                   <a class="category__main__grandchild__link-name" href="/categories/${child.id}">
+  //                                     <p>${child.category}</p>
+  //                                   </a>
+  //                                 </li>
+  //                               </div>`
+  //             $('.nav__category__child__list').append(insertHTML);
+  //           })
+  //         }
+  //         active_id = parent_id
+  //       });
+  //     }
+  //   })
+  //   $('.parent-list').on('mouseleave', function(){
+  //     $(this).css('background-color', 'white');
+  //     $(this).css('color', 'black');
+  //   });
+    // $(document).on('mouseleave', '#nav-category', function(){
+    //   $('.nav__category__parent__list').empty();
+    //   $('.nav__category__child__list').empty();
+    //   $('.nav__category__grandchild__list').empty();
+    // });
       
       
       
@@ -97,7 +91,11 @@ $(function(){
 
 
 
-
+  // $(".contents").on("mouseover", ".article", function(){
+  //   $(this).css({"background-color": "#F1940B", "font-weight": "bold"});
+  // }).on("mouseout", ".article", function(){
+  //   $(this).css({"background-color": "#FFFFFF", "font-weight": "normal"});
+  // })
 
 
   // var activeElement = ""
@@ -210,4 +208,4 @@ $(function(){
   //     });
   //   }
   // });
-});
+// });

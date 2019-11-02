fdc089348e327f03e76a859044a1bf1f320d6f83
moriokashingo <uma20056@yahoo.co.jp>
2019-11-02 10:41:29 +0900

    remove ancestry
    itemモデルのancestryカラムの削除

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,13 @@
 class Item < ApplicationRecord
 
-  has_many :comments               
+  has_many :comments
   belongs_to :user
   belongs_to :brand
   has_many :item_images                           , dependent: :destroy
   accepts_nested_attributes_for :item_images      , allow_destroy: true, reject_if: :all_blank
   belongs_to :category
   has_many :likes                                 , dependent: :destroy
-  has_one :transaction_informations 
-  has_ancestry
+  has_one :transaction_informations
 end
 
 

7ecc3e8cfca5eae3b94ef09727702ff757356e6e
moriokashingo <uma20056@yahoo.co.jp>
2019-11-02 22:44:07 +0900

    Update user.rb
    facebookの記述を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable,
-         omniauth_providers: [:google_oauth2]
+         omniauth_providers: [:google_oauth2,:facebook]
   has_many :likes       , dependent: :destroy
   has_many :items
   has_many :comments

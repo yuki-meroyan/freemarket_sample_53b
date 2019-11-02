1f8a705daf835d50bf72fd1403dd72970b1d2705
moriokashingo <uma20056@yahoo.co.jp>
2019-11-02 22:43:53 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {
     registrations: 'users/registrations',
     passwords: 'users/passwords',
-    omniauth_callbacks: 'users/omniauth_callbacks' 
+    omniauth_callbacks: 'users/omniauth_callbacks'
   }
 
   devise_scope :user do
